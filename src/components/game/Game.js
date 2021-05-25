95259224f4ec83d24ed6b3b865c19375eea0b0b1
Tony Bang <71351936+tonybvnq@users.noreply.github.com>
2021-05-25 09:58:51 +0200

    fix chat
    solves client #62

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -29,9 +29,9 @@ const styles = {
     emojiPicker: {
       position: "absolute",
       bottom: 60,
-      right: 30,
+      right: 50,
       cssFloat: "right",
-      marginLeft: "200px"
+      marginLeft: "100px"
     }
   };
 
@@ -666,8 +666,7 @@ submit(card){{
                                 this.textChat.map(chat => {
                                     return(
                                         <div className="msg-box">
-                                            <img className="user-img"
-                                                 src={require(`../../views/Images/Avatar/0.png`).default}/>
+ 
                                             <div className="flr">
                                                 <div className="messages">
                                                     <p className="msg" id="msg-0">
@@ -690,14 +689,6 @@ submit(card){{
                       value={this.state.text}
                       placeholder="Type a message and hit ENTER"
                       onChange={this.handleInputChange} />
-                    <button>
-                        <svg style={{width: '24px', height: '24px'}} viewBox="0 0 24 24">
-                            <path fill="rgba(0,0,0,.38)"
-                                  d="M17,12L12,17V14H8V10H12V7L17,12M21,16.5C21,16.88 20.79,17.21 20.47,17.38L12.57,21.82C12.41,21.94 12.21,22 12,22C11.79,22 11.59,21.94 11.43,21.82L3.53,17.38C3.21,17.21 3,16.88 3,16.5V7.5C3,7.12 3.21,6.79 3.53,6.62L11.43,2.18C11.59,2.06 11.79,2 12,2C12.21,2 12.41,2.06 12.57,2.18L20.47,6.62C20.79,6.79 21,7.12 21,7.5V16.5M12,4.15L5,8.09V15.91L12,19.85L19,15.91V8.09L12,4.15Z"/>
-                        </svg>
-                    </button>
-
-
                 </form>
             </section>
                 <div  style={{ color: "red", zIndex:'+1'}}>{errors}</div>
@@ -710,7 +701,7 @@ submit(card){{
                         />
                     </span>
                     ) : (
-                    <div style={{position: 'relative', top: '360px', right: '100px', zIndex: '+1'}}>
+                    <div style={{position: 'relative', top: '360px', right: '50px', zIndex: '+1'}}>
                     <p
                       style={styles.getEmojiButton}
                       onClick={() =>{
@@ -721,7 +712,7 @@ submit(card){{
                     </p>
                     </div>
                     )}
-                    <div style={{position: 'relative', top: '360px', right: '122px' }}>
+                    <div style={{position: 'relative', top: '360px', right: '72px' }}>
                      <p style={styles.getEmojiButton}
                       onClick={() =>{
                         this.displayEmojis()}}>
