7b1e0e709f3aeb1ae5fff7ddd2e69c3dd1c9ca64
elTorado <deanheizmann@gmx.ch>
2021-05-30 21:27:44 +0200

    blabla

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -131,7 +131,6 @@ class Game extends React.Component{
              if (!this.state.hasWon){
                 this.getHand();
              }
-            this.getHand();
             this.checkIfGameFinished();
             this.fetchData();
             this.getChatData();
